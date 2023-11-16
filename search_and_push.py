83907a22c0dff7221bcd9e680da5671a425c153d
Marcin_M <martinmalek1@gmail.com>
2023-11-16 14:45:16 -0500

    Update search_and_push.py
    Cleaned up unnecessary print statements and added a bit of commenting for clarification.

--- search_and_push.py
+++ search_and_push.py
@@ -2,21 +2,26 @@
 
 import requests, os
 
-folder = (r'C:\Users\Marcin Malek\Desktop\testing\tests')
+folder = (r'Absolute path goes here')
 os.chdir(folder)
 
+# Instantiating the necessarry lists and dictionaries.
 list_of_dicts_to_push = []
 title = {}
 name = {}
 date = {}
 feedback = {}
 
+# Iterates through the specified directory and appends the lines to the list_of_dicts_to_push variable 
+# **Maybe should put this into a function?
 for file in os.listdir(folder):
+    # Important to not forget this dictoinary inside the loop or it will give the incorrect information.
     information = []
     with open(file, 'r') as text:
         for line in text:
             line = line.strip('\n')
             information.append(line)
+    # Holder Dictionary for each loop.
     format_dict = {}            
     title['Title'] = information[0]
     name['Name'] = information[1]
@@ -28,8 +33,7 @@ for file in os.listdir(folder):
     format_dict.update(feedback)
     list_of_dicts_to_push.append(format_dict)
 
-print(list_of_dicts_to_push)
 for dictionary in list_of_dicts_to_push:
-    print(dictionary)
-    #requests.post("www.somewebsite.com", param=dict)
+    # Replace 'www.somewebsite.com' with the website you are trying to push to.
+    requests.post("www.somewebsite.com", param=dict)
     
